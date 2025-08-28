e44db92fed2ff6a869529f65238ed97b171015fa
Gal Shafir <shafir1999@gmail.com>
2025-08-28 13:16:22 +0300

    Use real data for analytics - 14

--- main.py
+++ main.py
@@ -11,15 +11,10 @@ CLIENT_PORT    = int(os.getenv("CLIENT_PORT", "8000"))
 LOG_LEVEL      = os.getenv("LOG_LEVEL", "INFO").upper()
 
 if __name__ == "__main__":
-    # Get the logging config file path
-    logging_config_path = os.path.join(os.path.dirname(__file__), "logging_config.json")
-    
-    # Run uvicorn with custom logging configuration
     uvicorn.run(
         "app.client:app", 
         host=CLIENT_HOST, 
         port=CLIENT_PORT, 
         reload=True, 
-        log_config=logging_config_path,
         log_level=LOG_LEVEL.lower()
     )
